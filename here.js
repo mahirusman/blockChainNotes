043a5bdded506009cfdfd5339786bf2d18054623
usman <m.usman@renesistech.com>
2024-01-29 22:18:06 +0500

    new changes

--- here.js
+++ here.js
@@ -10,3 +10,19 @@ person.sayHello(); // Outputs 'true'
 
 // this refer to the object that is function has property of
 //the scope of this keyword is determined at run time which is a dynamic scope
+
+setTimeout(() => {
+  console.log("hi");
+}, 0);
+
+setImmediate(() => {
+  console.log("how");
+}, 0);
+
+setTimeout(() => {
+  console.log("are");
+});
+
+// setImmediate(() => {
+//   console.log("how");
+// }, 0);
